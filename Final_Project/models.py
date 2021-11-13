7ca07d8ce10b57f88042df07f6f14d2d491ecb03
Maciej Bregisz <maciox55@gmail.com>
2021-11-13 00:53:24 -0500

    Added some missing model attributes

--- Final_Project/models.py
+++ Final_Project/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
 
 
 class Client(models.Model):
@@ -6,7 +8,6 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
-
 
 
 class SerialNumber(models.Model):
@@ -17,6 +18,7 @@ class SerialNumber(models.Model):
 
 
 class Shipment(models.Model):
+
     client = models.ForeignKey(Client, on_delete=models.CASCADE())
     shipping_vendor = models.CharField(max_length=200)
     part_number = models.CharField(max_length=200)
@@ -24,10 +26,17 @@ class Shipment(models.Model):
     sales_order = models.CharField(max_length=200)
     quantity = models.IntegerField
     serial_numbers = models.ForeignKey(SerialNumber)
+    processed = models.BooleanField
+    palletized = models.BooleanField
+    width = models.IntegerField
+    length = models.IntegerField
+    height = models.IntegerField
+    date_created = models.DateTimeField(default=now, editable=False)
+    date_processed = models.DateTimeField
+    date_shipping = models.DateDield
 
     def __str__(self):
         return self.client + self.work_order
-
 
     class Meta:
         ordering = ['name']
